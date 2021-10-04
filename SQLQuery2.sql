cbc8100764394526b126fb89a752444edc02a552
hassanarif1231 <49241912+hassanarif1231@users.noreply.github.com>
2021-10-04 11:14:12 +0500

    Add files via upload
    Latest Updated on 04-10-2021

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -201,11 +201,11 @@ AS
    Product_Size		   varchar(20),
    Product_Image       varchar(200),
    Department          int foreign key references department(Id),
-   Category		       int foreign key references category(Id)
+   Category		       int foreign key references category(Id),
 )
 
-create or alter procedure [dbo].[sp_AddProduct]
-
+create or alter procedure [dbo].[sp_Product_IUD]
+  @Product_Id           int,
   @Product_SKU		    varchar(20), 
   @Product_Name		    varchar(50), 
   @Product_Price	    decimal, 
@@ -215,13 +215,38 @@ create or alter procedure [dbo].[sp_AddProduct]
   @Product_Size		    varchar(20),
   @Product_Image        varchar(200),
   @Department           int, 
-  @Category		        int  
+  @Category		        int,
+  @iud_value		    char(1)	  
  
 AS
 
+ IF @iud_value = 'U'
+
   BEGIN
-      declare @Product_Id as BIGINT
-      insert into product(
+      
+      UPDATE product
+	  SET
+	   Product_SKU		   = @Product_SKU,		  
+	   Product_Name		   = @Product_Name,		  
+	   Product_Price	   = @Product_Price,	  
+	   Product_Description = @Product_Description,
+	   Product_Discount    = @Product_Discount ,   
+	   Product_Color       = @Product_Color,	  
+	   Product_Size		   = @Product_Size,		  
+	   Product_Image	   = @Product_Image,      
+	   Department		   = @Department,         
+	   Category			   = @Category	      
+	  
+       WHERE
+	      
+		  Product_Id = @Product_Id
+
+  END
+
+  IF @iud_value = 'I'  
+
+   BEGIN
+      INSERT into product(
 	   Product_SKU,		  
 	   Product_Name,		  
 	   Product_Price,	  
@@ -245,9 +270,16 @@ AS
 	   @Department,         
 	   @Category		      
 	  );
-	  Set @Product_Id = SCOPE_IDENTITY();
-	  Select @Product_Id as Id
+	 
 
+  END
+
+   IF @iud_value = 'D'  
+
+   BEGIN
+      DELETE FROM product
+	  WHERE 
+	  Product_Id = @Product_Id 
   END
 
   create or alter procedure [dbo].[sp_GetAllProducts]
@@ -257,53 +289,51 @@ AS
 
   BEGIN
      select * from 
-	 product p inner join category c 
+	 department d inner join category c 
   on 
-     p.Product_Id = c.Id inner join department d 
+     d.Id = c.Department inner join product p 
   on 
-     c.Id = d.Id
+      p.Category   = c.Id and
+	  p.Department = d.Id
   
   END
 
  create or alter procedure [dbo].[sp_GetProduct]
- @Id int 
+ @Product_Id int 
 
 AS
 
   BEGIN
-   
-    Select c.Id, d.Name, c.CatName from category c inner join department d on c.Department = d.Id where c.Id = @Id
-
+  Select 
+        p.Product_Id,
+		p.Product_Name,
+		p.Product_Price,
+		p.Product_Description,
+		p.Product_Discount,
+		p.Product_Color,
+		p.Product_Size,
+		p.Product_Image,
+		p.Product_SKU,
+		d.Id as Dep_Id,
+		d.Name as Dep_Name,
+		c.Id as Cat_Id,
+		c.CatName as Cat_Name
+   From
+        department d Inner Join 
+        category c
+   On
+        d.Id = c.Department Inner Join
+		product p
+   On
+        p.Category   = c.Id and
+		p.Department = d.Id
+   Where
+        p.Product_Id = @Product_Id
   END
 
 
-create or alter procedure [dbo].[sp_EditCategory]
- @Id int,
- @CatName varchar(50),
- @Department int
-
-AS
-
-  BEGIN
-      Update category 
-	  set 
-	  CatName	     = @CatName,
-	  Department = @Department
-	  Where
-	  Id = @Id
-  END
 
 
-  create procedure [dbo].[sp_DeleteCategory]
- @Id int
-
-AS
-
-  BEGIN
-      Delete from category 
-	  Where
-	  Id = @Id
-  END
 
 
 
@@ -313,3 +343,5 @@ AS
   select * from department
   select * from category
   select * from Product
+
+
